97a93ec78c58cdcc255702529d8681b384c760b0
youssef ashraf <64815036+etoo31@users.noreply.github.com>
2023-05-08 00:03:04 +0300

    Time: 0 ms (100.00%), Space: 11.6 MB (79.87%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,14 +1,14 @@
 class Solution {
 private:
     vector<string>ans;
+    string s;
 public:
     vector<string> generateParenthesis(int n) {
-        getAll(0 , 0 , n , "");
-        for (auto x : ans)
-            cout << x << endl;
+        s = "";
+        getAll(0 , 0 , n );
         return ans;
     }
-    void getAll(int open , int close , int n , string s)
+    void getAll(int open , int close , int n)
     {
         if (open == n && close == n)
         {
@@ -18,13 +18,13 @@ public:
         if (open < n)
         {
             s.push_back('(');
-            getAll(open +1 , close , n , s);
+            getAll(open +1 , close , n);
             s.pop_back();
         }
         if (close < open)
         {
             s.push_back(')');
-            getAll(open, close+1 , n , s);
+            getAll(open, close+1 , n);
             s.pop_back();
         }
         return;
