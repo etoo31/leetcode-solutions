27e3612541e5fbd9e36a10abbaad5013ec898fb2
youssef ashraf <64815036+etoo31@users.noreply.github.com>
2023-07-19 15:32:39 +0300

    Time: 267 ms (44.30%), Space: 23.8 MB (97.70%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-      vector<vector<int>> res;
-      int n = nums.size();
-      //sort the input vector
-      sort(nums.begin(), nums.end());
-
-      for (int i = 0; i < n - 2; i++) {
-        // check for duplicates
-          if (i > 0 && nums[i] == nums[i-1]) continue;
-          // calculating the target value
-          int target = nums[i]*-1;
-
-          int left = i + 1, right = n - 1;
-          while (left < right) {
-              int sum = nums[left] + nums[right];
-
-              if (sum == target) {
-                  res.push_back({nums[i], nums[left], nums[right]});
-
-                // check for duplicates
-               while (left < right && nums[left] == nums[left+1]) left++;
-               left++;
-        
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>>ret;
+        for (int x = 0 ; x < n; x++)
+        {
+            //cout << "x" << endl;
+            if (x && nums[x] == nums[x-1])continue;
+            int target = nums[x]*-1;
+            
+            int i = x+1, j = n-1;
+            while (i < j)
+            {
+                //cout << "i : " << i << "  j : " << j << endl; 
+                if (nums[i] + nums[j] == target)
+                {
+                    ret.push_back(vector<int>({nums[x] , nums[i] , nums[j]}));
+                    while (i < j && nums[i] == nums[i+1])
+                    {
+                        i++;
+                        cout << "i" << endl;
+                    }
+                    i++;
+                }
+                else if (nums[i] + nums[j] > target)
+                {
+                    j--;
+                }
+                else i++;
             }
+            
         }
-    }
-        return res;
+        return ret;
+        
     }
 };
