262098d1663f6b50882f61c78f472d50a687676b
youssef ashraf <64815036+etoo31@users.noreply.github.com>
2023-07-22 18:44:33 +0300

    Time: 4 ms (40.45%), Space: 6.4 MB (36.46%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,52 +2,21 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>st;
-        int n = s.size();
-        bool ans = true;
         for (int i = 0 ; i < s.size(); i++)
         {
-            if (s[i] == '(' || s[i] == '{' || s[i] == '[' )
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
                 st.push(s[i]);
-            else if (s[i] == ')')
+            else if (s[i] == ')' || s[i] == ']' || s[i] == '}')
             {
-                if (st.size() && st.top() == '(')
-                {
-                    st.pop();
-                }
-                else 
-                {
-                    ans = false;
-                    break;
-                }
-            }
-            else if (s[i] == '}')
-            {
-                if (st.size() && st.top() == '{')
-                {
-                    st.pop();
-                }
-                else
-                {
-                    ans = false;
-                    break;
-                }
-            }
-            else if (s[i] == ']')
-            {
-                if (st.size() && st.top() == '[')
-                {
-                    st.pop();
-                }
-                else
-                {
-                    ans = false;
-                    break;
-                }
-            }
-  
-    }
-        if (st.size())
-            ans = false;
-        return ans;
+                if (st.size() == 0)return false;
+                
+                char top = st.top();
+                if (s[i] == ']' && top != '[')return false;
+                else if (s[i] == ')' && top != '(')return false;
+                else if (s[i] == '}' && top != '{')return false;
+                st.pop();
+            }   
+        }
+        return (!st.size());
     }
 };
