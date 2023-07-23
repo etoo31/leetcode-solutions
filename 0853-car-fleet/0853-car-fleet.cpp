7cb1123ce463d8548455a0ceec0fa202fb8a3234
youssef ashraf <64815036+etoo31@users.noreply.github.com>
2023-07-23 22:08:05 +0300

    Time: 900 ms (5.01%), Space: 78.7 MB (48.84%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -2,34 +2,23 @@ class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
         vector<pair<int,int>>v;
-        stack<pair<int , int>>st;
-        
         int n = speed.size();
-        for (int i = 0 ;i  < n; i++)
+        for (int i = 0; i < n; i++)
         {
             v.push_back({position[i] , speed[i]});
         }
         sort(v.begin() , v.end());
-        for (int i = n-1 ; i >= 0; i--)
+        stack<pair<int,double>>st;
+        for (int i = n-1; i >= 0; i--)
         {
-            //cout << v[i].first << " " << v[i].second << endl;
-            if (st.size() == 0 || st.top().second >= v[i].second)
+            double reachTime = (double)(target - v[i].first)/(double)v[i].second;
+            cout << v[i].first << " " << reachTime << endl;
+            if (!st.empty() && reachTime <= st.top().second)
             {
-                st.push({v[i].first , v[i].second});
+                continue;
             }
-            else if (st.top().second < v[i].second)
-            {
-                auto lastCar = st.top();
-                double sec1 = abs((double)(target - lastCar.first)/(double)(lastCar.second));
-                double sec2 = abs((double)(target - v[i].first)/((double)v[i].second));
-                //cout << sec1 << " " << sec2 << endl;
-                if (sec1 < sec2)
-                {
-                    st.push({v[i].first , v[i].second});
-                }
-            }
+            st.push({v[i].first , reachTime});
         }
         return st.size();
-        
     }
 };
