52f59b694b4bd5305eaf1314d6ec19beae950162
youssef ashraf <64815036+etoo31@users.noreply.github.com>
2023-07-26 18:09:18 +0300

    Time: 56 ms (17.71%), Space: 89.3 MB (74.54%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -13,12 +13,12 @@ public:
             int A2 = getValue(i+1 , nums1);
             int B1 = getValue(j , nums2);
             int B2 = getValue(j+1 , nums2);
-            cout << A1 << " " << B2 << " " << B1 <<" " <<  A2 << endl;
-            cout << "i = " << i << "   j = " << j << endl;
+            //cout << A1 << " " << B2 << " " << B1 <<" " <<  A2 << endl;
+            //cout << "i = " << i << "   j = " << j << endl;
             if (A1 <= B2 && B1 <= A2)
             {
-                cout << "In" << endl;
-                cout << A1 << " " << B2 << endl;
+                //cout << "In" << endl;
+                //cout << A1 << " " << B2 << endl;
                 if (total % 2)
                 {
                     return (double)min(B2, A2);
