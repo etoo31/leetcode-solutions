486101948b86a95a397a695660b1faa7fceb4ad0
youssef ashraf <64815036+etoo31@users.noreply.github.com>
2023-07-26 18:08:37 +0300

    Time: 23 ms (99.27%), Space: 89.3 MB (74.54%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,52 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>v;
-        int i = 0, j = 0;
-        while (i < nums1.size() || j < nums2.size())
+        int total = nums1.size() + nums2.size();
+        int half = total/2;
+        int s= 0 , e = nums1.size()-1;
+        while (true)
         {
-            if (i == nums1.size())
+            cout << s << " " << e << endl;
+            int i = (s + e) /2;
+            int j = half - i - 2;
+            int A1 = getValue(i , nums1);
+            int A2 = getValue(i+1 , nums1);
+            int B1 = getValue(j , nums2);
+            int B2 = getValue(j+1 , nums2);
+            cout << A1 << " " << B2 << " " << B1 <<" " <<  A2 << endl;
+            cout << "i = " << i << "   j = " << j << endl;
+            if (A1 <= B2 && B1 <= A2)
             {
-                v.push_back(nums2[j]);
-                j++;
+                cout << "In" << endl;
+                cout << A1 << " " << B2 << endl;
+                if (total % 2)
+                {
+                    return (double)min(B2, A2);
+                }
+                else return (max(B1, A1)+min(B2, A2))/2.0;
             }
-            else if (j == nums2.size())
+            else if (A1 > B2)
             {
-                v.push_back(nums1[i]);
-                i++;
+                if (e == i-1)
+                    e = i - 2;
+                else e = i - 1;
             }
-            else if (nums2[j] < nums1[i])
-            {
-                v.push_back(nums2[j]);
-                j++;
-            }
-            else {
-                v.push_back(nums1[i]);
-                i++;
-
-            }
+            else s = i + 1;
         }
-        int n = v.size();
-        if (n%2)
+        if (total%2)
         {
-            return (double)v[n/2];
+            return (double)nums2[half];
         }
-        else 
-            return (v[n/2]+ v[(n/2)-1])/2.0;
-        //return 2.000000;
+        else return (nums2[half]+nums2[half-1])/2.0;
+    }
+    int getValue(int index , vector<int>&v)
+    {
+        if (index < 0)
+        {
+            return -10000000;
+        }
+        else if (index >= v.size())
+            return 10000000;
+        return v[index];
     }
 };
