e70ce0b0545a74c70d63520c804f518c1b92ed12
youssef ashraf <64815036+etoo31@users.noreply.github.com>
2023-07-26 18:10:15 +0300

    Time: 16 ms (99.89%), Space: 89.2 MB (74.54%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -6,19 +6,15 @@ public:
         int s= 0 , e = nums1.size()-1;
         while (true)
         {
-            //cout << s << " " << e << endl;
             int i = (s + e) /2;
             int j = half - i - 2;
             int A1 = getValue(i , nums1);
             int A2 = getValue(i+1 , nums1);
             int B1 = getValue(j , nums2);
             int B2 = getValue(j+1 , nums2);
-            //cout << A1 << " " << B2 << " " << B1 <<" " <<  A2 << endl;
-            //cout << "i = " << i << "   j = " << j << endl;
+           
             if (A1 <= B2 && B1 <= A2)
             {
-                //cout << "In" << endl;
-                //cout << A1 << " " << B2 << endl;
                 if (total % 2)
                 {
                     return (double)min(B2, A2);
@@ -33,7 +29,6 @@ public:
             }
             else s = i + 1;
         }
-        return 1;
     }
     int getValue(int index , vector<int>&v)
     {
