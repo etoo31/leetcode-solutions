ff4d10cc0404b22ee806e131a0f400d50de23846
youssef ashraf <64815036+etoo31@users.noreply.github.com>
2024-04-28 16:44:16 +0300

    Time: 18 ms (10.03%), Space: 22.2 MB (82.56%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,31 +2,34 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int>left(n) , right(n);
-        for (int i = 0 ; i < n; i++)
-        {
-            if (!i)
-            {
-                left[i] = height[i];
-                continue;
-            }
-            left[i] = max(left[i-1] , height[i]);
-        }
-        for (int i = n-1; i >= 0 ; i--)
-        {
-            if (i == n-1)
-            {
-                right[i] = height[i];
-                continue;
-            }
-            right[i] = max(height[i] , right[i+1]);
-        }
+        int maxL = height[0] , maxR = height[n-1];
+        int l = 0 , r = n-1;
         int ans = 0;
-        for (int i = 1 ; i < n-1; i++)
+        char which;
+        
+        while (l < r)
         {
-            int mini = min(left[i-1] , right[i+1]);
-            ans += max(0 , mini - height[i]);
+            if (which == 'L')
+            {
+                ans += max(0 , maxL - height[l]);
+                maxL = max(maxL , height[l]);
+            }
+            else if (which =='R')
+            {
+                ans += max(0 , maxR - height[r]);
+                maxR = max(maxR , height[r]);
+            }
+            if (maxL > maxR)
+             {
+                r--;
+                which ='R';
+             }
+            else {
+                l++;
+                which = 'L';
+            }
         }
         return ans;
+        
     }
 };
