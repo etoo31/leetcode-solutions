a6937091699562505b327d9e8fbec396bd17a2fb
youssef ashraf <64815036+etoo31@users.noreply.github.com>
2024-05-02 00:27:46 +0300

    Time: 138 ms (16.03%), Space: 109.9 MB (10.43%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,18 +3,22 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
         vector<int>ans(n , 0);
-        stack<pair<int,int>>st;
+        stack<pair<int , int>>st;
         for (int i = 0 ; i < n; i++)
         {
-            int temp = temperatures[i];
-            while (!st.empty() && temp > st.top().first)
+            if (!i)
             {
-                ans[st.top().second] = i - st.top().second;
-                st.pop();
+                st.push({temperatures[i] , i});
+                continue;
             }
-            st.push({temp , i});
+            while(!st.empty() && st.top().first < temperatures[i])
+            {
+                auto temp =  st.top();
+                st.pop();
+                ans[temp.second] = i-temp.second;
+            }
+            st.push({temperatures[i] , i});
         }
         return ans;
-    
     }
 };
