d20eda0c16db8ff48bd49e5ee4fb24b92391ccd1
youssef ashraf <64815036+etoo31@users.noreply.github.com>
2024-05-05 15:53:36 +0300

    Time: 0 ms (100.00%), Space: 12.6 MB (19.13%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -5,17 +5,15 @@ public:
         int ans = nums[0];
         while (s <= e)
         {
-            //cout << s << " " << e << endl;
             int mid = (s+e)/2;
-            if (nums[mid] < ans)
-            {
-                e = mid -1;
+            if (nums[mid] < ans){
                 ans = nums[mid];
+                e = mid -1;   
             }
-            else s = mid + 1;
-            //cout <<"after "<<  s << " " << e << endl;
+            else {
+                s = mid +1;
+            }
         }
         return ans;
-        
     }
 };
